39dfec9707bafea278c6ba51e1d2e457f741df90
Arjun Akkiraju <20arjuna@students.harker.org>
2019-03-09 15:45:45 -0800

    testing loading

--- Extension/script.js
+++ Extension/script.js
@@ -3,3 +3,10 @@ chrome.tabs.getSelected(null,function(tab) {
     var site = tablink.hostname;
     document.getElementById("sitename").innerHTML = site + " may be trying to pull a fast one on you!";
 })
+
+function loading()
+{
+  console.log("Loading baby");
+}
+
+document.getElementById('analyze').addEventListener('click', loading)
