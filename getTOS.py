cbf1fe807055d88fe9060963551732dc51f4ec6c
Arjun Akkiraju <20arjuna@students.harker.org>
2019-03-09 17:24:03 -0800

    google search fo tos need to hardcode google

--- getTOS.py
+++ getTOS.py
@@ -1,19 +1,11 @@
-# from googlesearch import search
-# print('running')
-# query = "google.com privacy policy"
-# resultList = []
-# print('entering loop now!')
-# for j in search(query, tld="co.in", num=10, stop=1, pause=2):
-#     resultList.append(j)
-#     print(j)
-import requests
-from bs4 import BeautifulSoup
+from googlesearch import search
+print('running')
+query = "google.com privacy policy"
+resultList = []
+print('entering loop now!')
+for url in search('redis terms of service', tld='com', lang='en', stop=5):
+    print(url)
 
-text = "google privacy policy"
-url = "https://www.google.com/search?q=" + text
-
-response = requests.get(url)
-print(response)
 # soup = BeautifulSoup(response.text,"lxml")
 # for item in soup.select(".r a"):
 #     f_url = item.get('href')
